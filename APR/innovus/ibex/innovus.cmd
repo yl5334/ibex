f199af9a98c79e3515a62a9ef13d6ea1e894283f
yl5334 <yl5334@columbia.edu>
2024-04-09 16:37:47 -0400

    APR

---
+++ APR/innovus/ibex/innovus.cmd
@@ -0,0 +1,18 @@
+#######################################################
+#                                                     
+#  Innovus Command Logging File                     
+#  Created on Tue Apr  9 16:31:31 2024                
+#                                                     
+#######################################################
+
+#@(#)CDS: Innovus v21.16-s078_1 (64bit) 12/07/2022 12:07 (Linux 3.10.0-693.el7.x86_64)
+#@(#)CDS: NanoRoute 21.16-s078_1 NR221206-1807/21_16-UB (database version 18.20.600) {superthreading v2.17}
+#@(#)CDS: AAE 21.16-s035 (64bit) 12/07/2022 (Linux 3.10.0-693.el7.x86_64)
+#@(#)CDS: CTE 21.16-s024_1 () Dec  5 2022 05:41:45 ( )
+#@(#)CDS: SYNTECH 21.16-s009_1 () Nov  9 2022 03:47:50 ( )
+#@(#)CDS: CPE v21.16-s066
+#@(#)CDS: IQuantus/TQuantus 21.1.1-s939 (64bit) Wed Nov 9 09:34:24 PST 2022 (Linux 3.10.0-693.el7.x86_64)
+
+set_global _enable_mmmc_by_default_flow      $CTE::mmmc_default
+suppressMessage ENCEXT-2799
+win
